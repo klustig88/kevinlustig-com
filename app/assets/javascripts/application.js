d5086a3b45259fd767a140022d32c55a6e0c7983
Kevin Lustig <klustig@Kevins-MacBook-Pro-2.local>
2014-01-03 15:25:28 -0600

    fixed tabbing logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
   $('.tab').hide();
+  $('#tab1').show();
+  $('.tabs li a').click(function() {
+    $('.tab').hide();
+    $('.tabs li').removeClass( 'active' )
+    $(this).parent().addClass('active')
+    $($(this).attr("href")).show();
+  });
  
 });
